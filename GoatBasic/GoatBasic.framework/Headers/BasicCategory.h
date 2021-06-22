38da4ee825dc6d96ec6e4352ee630b2c96a8f71e
solo <lixiaojian@goatgames.com>
2021-06-22 09:57:37 +0800

    提交

---
+++ GoatBasic/GoatBasic.framework/Headers/BasicCategory.h
@@ -0,0 +1,21 @@
+//
+//  BasicCategory.h
+//  GoatBasic
+//
+//  Created by choici o on 2021/1/30.
+//
+
+#ifndef BasicCategory_h
+#define BasicCategory_h
+
+#import <GoatBasic/NSDate+Basic.h>
+#import <GoatBasic/NSString+Basic.h>
+#import <GoatBasic/UIView+Basic.h>
+#import <GoatBasic/NSDictionary+Basic.h>
+#import <GoatBasic/NSData+Basic.h>
+#import <GoatBasic/NSObject+Basic.h>
+#import <GoatBasic/UIViewController+Basic.h>
+#import <GoatBasic/UIAlertController+Basic.h>
+#import <GoatBasic/UIDevice+Basic.h>
+
+#endif /* BasicCategory_h */
---
+++ GoatBasic/GoatBasic.framework/Headers/GoatBasic.h
@@ -0,0 +1,30 @@
+//
+//  GoatBasic.h
+//  GoatBasic
+//
+//  Created by choici o on 2021/1/28.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <GoatBasic/BasicToast.h>
+#import <GoatBasic/BasicNetwork.h>
+#import <GoatBasic/BasicFileManager.h>
+#import <GoatBasic/BasicViewFromNib.h>
+#import <GoatBasic/BasicSecurity.h>
+#import <GoatBasic/BasicMsgSend.h>
+#import <GoatBasic/BasicCategory.h>
+#import <GoatBasic/BasicUUID.h>
+#import <GoatBasic/BasicKeychain.h>
+#import <GoatBasic/BasicSystemConfiguration.h>
+#import <GoatBasic/BasicMacro.h>
+
+
+//! Project version number for GoatBasic.
+FOUNDATION_EXPORT double GoatBasicVersionNumber;
+
+//! Project version string for GoatBasic.
+FOUNDATION_EXPORT const unsigned char GoatBasicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoatBasic/PublicHeader.h>
+
